cffeb8d6fa4657cd610c0ec9d3c3537ab049eb00
David Amelekh <david.amelekh@protonmail.com>
2026-06-09 17:24:03 +0200

    Adopt new hexagonal mark; add sitemap, robots.txt, JSON-LD, PNG OG image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://orogen.network",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   build: {
     format: "file",
   },
